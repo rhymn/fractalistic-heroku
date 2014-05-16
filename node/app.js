eabcd62df14aa9a66c878209561aa370e03cae55
david <david@kaustik.com>
2014-05-16 13:10:18 +0200

    Plockat bort route "user"

--- node/app.js
+++ node/app.js
@@ -5,7 +5,6 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -28,7 +27,6 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 app.get('/setsettings', routes.setsettings);
 app.get('/getsettings', routes.getsettings);
